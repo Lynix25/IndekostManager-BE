395b1dbc0849477264f580515f5fe156ee861933
Meiriska Amelia <kim07.my231@gmail.com>
2023-01-10 18:32:04 +0700

    Enhance Role

--- src/main/java/com/indekos/repository/RoleRepository.java
+++ src/main/java/com/indekos/repository/RoleRepository.java
@@ -15,8 +15,7 @@ public interface RoleRepository extends JpaRepository<Role, String> {
 	@Query(value = "SELECT * FROM role WHERE is_deleted IS FALSE ORDER BY name ASC", nativeQuery = true)
 	List<Role> findAllActiveByOrderByNameAsc();
 	
-	@Query(value = "SELECT * FROM role WHERE name LIKE :role_name", nativeQuery = true)
-	Role findByName(@Param("role_name") String roleName);
+	Role findByName(String roleName);
 	
 	@Query(value = "SELECT * FROM role WHERE name LIKE :role_name AND id NOT LIKE :role_id", nativeQuery = true)
 	Role findByNameAndIdNot(@Param("role_name") String roleName, @Param("role_id") String roleId);
