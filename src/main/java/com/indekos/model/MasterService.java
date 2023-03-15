3daa291e2bae95132058f81a9523b862b10029a8
Meiriska Amelia <kim07.my231@gmail.com>
2023-03-15 17:32:30 +0700

    Adjust Model MasterService

--- src/main/java/com/indekos/model/MasterService.java
+++ src/main/java/com/indekos/model/MasterService.java
@@ -5,12 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Data
 @Entity
 @AllArgsConstructor @NoArgsConstructor
 public class MasterService extends AuditableEntity {
-    private String name;
+    
+	@Column(unique = true, nullable = false)
+	private String name;
+	
+    @Column(columnDefinition = "text")
     private String description;
+    
+    @Column(nullable = false, columnDefinition = "boolean default false")
+	private boolean isDeleted;
 }
