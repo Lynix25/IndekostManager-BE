2baf1389a5932eb89f57c0bd5199f7b828d4f76f
Meiriska Amelia <kim07.my231@gmail.com>
2023-03-16 21:34:35 +0700

    Update UserRepository.java

--- src/main/java/com/indekos/repository/UserRepository.java
+++ src/main/java/com/indekos/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.indekos.repository;
 
-import com.indekos.dto.UserAddRoomId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import com.indekos.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
