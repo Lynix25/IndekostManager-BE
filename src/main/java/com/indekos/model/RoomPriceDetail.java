be18e82444248c0ca3c23735b0e2c20e5bc4e907
Meiriska Amelia <kim07.my231@gmail.com>
2023-03-25 14:30:06 +0700

    Add room detail: detail and price

--- src/main/java/com/indekos/model/RoomPriceDetail.java
+++ src/main/java/com/indekos/model/RoomPriceDetail.java
@@ -1,19 +1,35 @@
 package com.indekos.model;
 
-import com.indekos.common.base.entity.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@EqualsAndHashCode(callSuper = false)
 @Data
 @Entity
 @AllArgsConstructor @NoArgsConstructor
-public class RoomPriceDetail extends BaseEntity {
+public class RoomPriceDetail {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
     private Integer capacity;
     private Integer price;
 
-//    @ManyToOne
-//    private Room room;
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", referencedColumnName = "id")
+    private Room room;
 }
