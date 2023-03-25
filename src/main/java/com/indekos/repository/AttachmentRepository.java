1c9dc2bf24f0fb65cf402c0aa493241f28bbbe34
Meiriska Amelia <kim07.my231@gmail.com>
2023-03-25 13:57:53 +0700

    Delete unused file

--- src/main/java/com/indekos/repository/AttachmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.indekos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.indekos.model.Attachment;
-
-public interface AttachmentRepository extends JpaRepository<Attachment, String> {
-	
-}
