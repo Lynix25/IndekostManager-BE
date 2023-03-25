c9687b99725f5d1a6f2fd0e078eca4576d2ee962
Meiriska Amelia <kim07.my231@gmail.com>
2023-03-25 14:24:09 +0700

    Add user detail: Multifile user document

---
+++ src/main/java/com/indekos/model/UserDocument.java
@@ -0,0 +1,34 @@
+package com.indekos.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.indekos.common.base.entity.BaseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+public class UserDocument extends BaseEntity {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Lob
+	@Column(length = 1000)
+	private byte[] identityCardImage;
+	
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
---
+++ src/main/java/com/indekos/repository/UserDocumentRepository.java
@@ -0,0 +1,12 @@
+package com.indekos.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.indekos.model.User;
+import com.indekos.model.UserDocument;
+
+public interface UserDocumentRepository extends JpaRepository<UserDocument, String> {
+	List<UserDocument> findByUser(User user);
+}
